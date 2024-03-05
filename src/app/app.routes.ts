3ed7a389f9d9a9c314431512fb95e136545a803c
DushanMalind <dushanmalinda010@gmail.com>
2024-03-05 19:57:49 +0530

    About component Data Add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./page/home/home.component";
+import {AboutComponent} from "./page/about/about.component";
 
 export const routes: Routes = [
   { path: 'home', 'title': "Home", component: HomeComponent },
+  { path: 'about', 'title': "About", component: AboutComponent },
   { path: '', redirectTo: "/home", pathMatch: "full" },
 
 ];
