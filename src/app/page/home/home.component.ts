7ac9c7fa195ce0ad77d5312327fab02e662ba8a7
DushanMalind <dushanmalinda010@gmail.com>
2024-03-05 19:49:41 +0530

    Home Add Content Ok

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import {NgIcon} from "@ng-icons/core";
+import {MatIconModule} from '@angular/material/icon';
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    NgIcon
+    MatIconModule,
+    FontAwesomeModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
