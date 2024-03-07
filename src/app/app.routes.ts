26aba14f339ed806c3b935cce29a3dc562a0ade2
DushanMalind <dushanmalinda010@gmail.com>
2024-03-07 08:07:26 +0530

    Contact Rotes Done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./page/home/home.component";
 import {AboutComponent} from "./page/about/about.component";
+import {ContactComponent} from "./page/contact/contact.component";
 
 export const routes: Routes = [
   { path: 'home', 'title': "Home", component: HomeComponent },
   { path: 'about', 'title': "About", component: AboutComponent },
+  { path: 'contact', 'title': "Contact", component: ContactComponent },
   { path: '', redirectTo: "/home", pathMatch: "full" },
 
 ];
